79b437629bae6735c8f78c95800609f56a33184f
BigShow1949 <1029883589@qq.com>
2016-04-26 13:45:27 +0800

    Initial Commit

---
+++ YFPushTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFPushTest
+//
+//  Created by zhht01 on 16/4/26.
+//  Copyright © 2016年 BigShow1949. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YFPushTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFPushTest
+//
+//  Created by zhht01 on 16/4/26.
+//  Copyright © 2016年 BigShow1949. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
