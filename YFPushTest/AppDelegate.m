454e43f95e53e1e780b7b40ba1164650ea2539a9
BigShow1949 <1029883589@qq.com>
2016-07-25 11:56:02 +0800

    苹果推送测试
    苹果推送测试~~

--- YFPushTest/AppDelegate.m
+++ YFPushTest/AppDelegate.m
@@ -15,31 +15,137 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    //    application.applicationIconBadgeNumber = 0;
+    
+    if ([UIDevice currentDevice].systemVersion.doubleValue >= 8.0)
+    {
+        // 1.请求授权可以给用户发送通知
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeSound | UIUserNotificationTypeBadge | UIUserNotificationTypeAlert categories:nil];
+        
+        [application registerUserNotificationSettings:settings];
+    }
+    else
+    {
+        [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound];
+    }
+    
+    if (launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey])
+    {
+        // 跳转
+    }
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+/**
+ *  远程推送注册成功
+ *
+ *  @param deviceToken deviceToken
+ */
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // 25bb75ac 3ffcebd7 90d9f517 1ebca904 154a367a 87781e5d b9ea288e 37fdf487
+    NSLog(@"-----deviceToken ---- ---- %@ devDesc ---%@", deviceToken,deviceToken.description);
+    
+    //tokenStr 得到可用的token。
+    NSString *tokenStr = [NSString stringWithFormat:@"%@",deviceToken];
+    tokenStr = [tokenStr stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];//将其中的<>去掉
+    tokenStr = [tokenStr stringByReplacingOccurrencesOfString:@" " withString:@""];//将其中的空格去掉
+    
+    NSLog(@"token--- %@",tokenStr);
+//    //注册成功，返回token
+//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"APNS返回的Token:" message:tokenStr delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+//    
+//    [alert show];
+    
+    //    applicationIconBadgeNumber
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+/**
+ *  注册失败
+ *
+ */
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    
+    NSLog(@"注册失败 ---- %@",error);
+    NSString *tokenStr = [NSString stringWithFormat:@"%@",error];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"注册失败!" message:tokenStr delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+    [alert show];
+    NSLog(@"注册失败%@",error);
+    
+    
+}//NS_AVAILABLE_IOS(3_0);
+
+
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"接收到推送通知 ---- %@", userInfo);
+    
+    /*
+     
+     "name" : "YFPushTest",
+     "action_type" : "1",
+     "message" : "您的车辆京KKKKKK于2016-4-22 驶入邯郸",
+     "aps" : {
+        "alert" : "This is some fancy message.",
+        "badge" : 1,
+        "sound" :  "您的车辆京KKKKKK于2016-4-22 驶入邯郸"
+     };
+     
+     */
+    
+    //    application.applicationIconBadgeNumber -=1;
+    //将推送消息以alert形式呈现
+    NSString *message = [[userInfo objectForKey:@"aps"]objectForKey:@"alert"];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:message delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+    
+    [alert show];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
+// This callback will be made upon calling -[UIApplication registerUserNotificationSettings:]. The settings the user has granted to the application will be passed in as the second argument.
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+    
+    NSLog(@"ls---- %@",notificationSettings);
+    
+    // 2.注册远程通知
+    [application registerForRemoteNotifications];
+    
+}//NS_AVAILABLE_IOS(8_0) __TVOS_PROHIBITED;
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
+
+// Called when your app has been activated by the user selecting an action from a remote notification.
+// A nil action identifier indicates the default action.
+// You should call the completion handler as soon as you've finished handling the action.
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(nullable NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo withResponseInfo:(NSDictionary *)responseInfo completionHandler:(void(^)())completionHandler
+{
+    
+    NSLog(@"ios9新增 --- %@ -- %@ --- %@",identifier,userInfo,responseInfo);
+    
+    
+}//NS_AVAILABLE_IOS(9_0) __TVOS_PROHIBITED;
+
+
+// Called when your app has been activated by the user selecting an action from a remote notification.
+// A nil action identifier indicates the default action.
+// You should call the completion handler as soon as you've finished handling the action.
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(nullable NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void(^)())completionHandler
+{
+    
+    NSLog(@"ios8新增 --- %@ -- %@ --- %@",identifier,userInfo,application);
+    
+    
+}//NS_AVAILABLE_IOS(8_0) __TVOS_PROHIBITED;
+
+
+
 
 @end
--- YFPushTest/ViewController.m
+++ YFPushTest/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSString *str = [[NSBundle mainBundle] bundleIdentifier];
+    NSLog(@"str = %@", str);
 }
 
 - (void)didReceiveMemoryWarning {
